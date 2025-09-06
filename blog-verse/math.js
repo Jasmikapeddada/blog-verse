abe6a52b7a1345ea7bcb5105782e438f269247c1
Jasmika Peddada <you@example.com>
2025-09-06 16:43:40 +0530

    Commited

---
+++ blog-verse/math.js
@@ -0,0 +1,19 @@
+export const add = (x, y) => {
+    return x + y;
+}
+console.log(add(10, 12));
+
+export const sub = (x, y) => {
+    return x - y;
+}
+console.log(sub(10, 12));
+
+export const mul = (x, y) => {
+    return x * y;
+}
+console.log(mul(10, 12));
+
+export const div = (x, y) => {
+    return x / y;
+}
+console.log(div(10, 12));
